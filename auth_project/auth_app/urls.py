942bf62266d76c3503417399501a68be952dc32e
hojat-gazestani <HojatGazestani@gmail.com>
2023-05-26 15:30:29 +0330

    Added hostname and port number ot home

--- auth_project/auth_app/urls.py
+++ auth_project/auth_app/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.auth, name='auth'),
     path('auth', views.auth, name='auth'),
-    path('auth1/', views.auth1, name='auth1'),
-    path('auth2/', views.auth2, name='auth2'),
+    path('auth1', views.auth1, name='auth1'),
+    path('auth2', views.auth2, name='auth2'),
 ]
